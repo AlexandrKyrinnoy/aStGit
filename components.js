61c5680c33fb0be0deafc791c747b5da77f6fdef
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-07-20 16:53:26 +0300

    В ClassHelperForSpeak добавлены функции, работающие через DOM

--- components.js
+++ components.js
@@ -6,18 +6,44 @@ var ClassHelperForSpeak = {
 	
 	/*METHODS*/
 	sayHello : function(targetDivId) {
-		/*1 - ƒобавить позже - ≈сли по указанному id не найден жлемент на странице - возвращать "-1", если все удачно - "0" */
-		/*2 - переделать через DOM добавление и удаление, чтобы создавались новые span с текстом и добавл€лись в current <div>*/
-		var obj_targetDivId = 	document.getElementById(targetDivId);
-		obj_targetDivId.innerHTML = "Hello, friend!";
+		this.say(targetDivId, "Hello, friend!");
 	},
 	sayGoodBye : function(targetDivId) {
-		var obj_targetDivId = 	document.getElementById(targetDivId);
-		obj_targetDivId.innerHTML = "Goodbye.";
+		/*need removed this method*/
+		this.say(targetDivId, "Goodbye.");
+	},
+	sayWithDOM : function(targetDivId, added_text) {
+		var obj_targetDivId = document.getElementById(targetDivId);
+		var obj_targetDivId_Child = document.createElement('span');
+		obj_targetDivId_Child.innerHTML = added_text;
+		obj_targetDivId.appendChild(obj_targetDivId_Child);
 	},
 	cleaning : function(targetDivId) {
+		/*need removed this method*/
 		var obj_targetDivId = document.getElementById(targetDivId);
 		obj_targetDivId.innerHTML = "";
+	},
+	cleaninWithDOM : function(targetDivId) {
+		var obj_targetDivId = document.getElementById(targetDivId);
+		var obj_targetDivId_Child;
+		if ( obj_targetDivId.childElementCount > 0) {
+			for (i=obj_targetDivId.childElementCount; i>0; i--) {
+				obj_targetDivId_Child = obj_targetDivId.children[i-1];
+				obj_targetDivId.removeChild(obj_targetDivId_Child);
+			}
+		} else {
+			return -1;
+		}
+	},
+	say : function(targetDivId, added_text) {
+		var obj_targetDivId = document.getElementById(targetDivId);
+		if (obj_targetDivId != undefined) {
+			obj_targetDivId.innerHTML = added_text;
+			return 0;
+		} else {
+			return -1;
+		}
+		
 	}
 	
 	
